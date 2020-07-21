9a690cc3e35abce4dc4059584fb37f993ad5e25b
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-21 01:51:47 -0300

    feature: developing the server activation client

--- src/main/java/com/jhonatan/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/jhonatan/algafood/di/service/AtivacaoClienteService.java
@@ -1,21 +1,23 @@
 package com.jhonatan.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.jhonatan.algafood.di.model.Cliente;
+import com.jhonatan.algafood.di.notificacao.NivelUrgencia;
 import com.jhonatan.algafood.di.notificacao.Notificador;
+import com.jhonatan.algafood.di.notificacao.TipoDoNotificador;
 
 @Component
 public class AtivacaoClienteService {
 	
+	@TipoDoNotificador(NivelUrgencia.URGENTE)
+	@Autowired
 	private Notificador notificador;
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	}
-
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
+		
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 }
