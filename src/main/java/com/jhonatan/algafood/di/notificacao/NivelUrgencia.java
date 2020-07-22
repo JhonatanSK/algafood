0cc1c1fab9e34a08fa0b7adf332b313db1cab4ac
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-22 05:19:38 -0300

    feature: developing the classes with entity atributes for Restaurante, Cozinha and developing some methods cruds with jpa and hibernate

--- src/main/java/com/jhonatan/algafood/di/notificacao/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.jhonatan.algafood.di.notificacao;
-
-public enum NivelUrgencia {
-
-	URGENTE,
-	SEM_URGENCIA
-}
--- src/main/java/com/jhonatan/algafood/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.jhonatan.algafood.di.notificacao;
-
-import com.jhonatan.algafood.di.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
