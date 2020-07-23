014f4c5d95211f5b96793c6a10fd4371a962f534
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-23 00:24:53 -0300

    developing the domain and infra layer for the entity Estado

---
+++ src/main/java/com/jhonatan/algafood/domain/repository/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.jhonatan.algafood.domain.repository;
+
+import java.util.List;
+
+import com.jhonatan.algafood.domain.model.Estado;
+
+public interface EstadoRepository{
+	List<Estado> listar();
+	Estado buscar(Long id);
+	Estado adicionar(Estado estado);
+	void remover(Estado estado);
+}
