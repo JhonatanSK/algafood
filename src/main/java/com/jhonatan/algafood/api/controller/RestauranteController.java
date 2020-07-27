33cc833c55a5760fdd63fc6ab7aa527d36447b35
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-27 05:25:49 -0300

    feature: developing the controller Restaurante with HTTP methods for the application

--- src/main/java/com/jhonatan/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/jhonatan/algafood/api/controller/RestauranteController.java
@@ -1,11 +1,15 @@
 package com.jhonatan.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jhonatan.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.jhonatan.algafood.domain.model.Restaurante;
 import com.jhonatan.algafood.domain.repository.RestauranteRepository;
@@ -66,6 +71,33 @@ public class RestauranteController {
 		}
 	}
 	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+			@RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if (restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		merge(campos, restauranteAtual);
+		
+		return atualizar(restauranteAtual, restauranteId);
+	}
+	
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		dadosOrigem.forEach((nomeProprietario, valorProprietario) -> {
+			Field field = ReflectionUtils.findField(Restaurante.class, nomeProprietario);
+			field.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
+	}
 	
 	
 }
