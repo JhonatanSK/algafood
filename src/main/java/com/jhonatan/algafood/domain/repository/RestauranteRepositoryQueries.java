3591d8deee830835f0c42e780ff1859762c68dd5
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2021-04-02 15:11:12 -0300

    feat: developing the interface RestauranteRepository

--- src/main/java/com/jhonatan/algafood/domain/repository/RestauranteRepositoryQueries.java
+++ src/main/java/com/jhonatan/algafood/domain/repository/RestauranteRepositoryQueries.java
@@ -8,5 +8,4 @@ import com.jhonatan.algafood.domain.model.Restaurante;
 public interface RestauranteRepositoryQueries {
 	List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFretefinal);
 
-	List<Restaurante> findComFreteGratis(String nome);
 }
